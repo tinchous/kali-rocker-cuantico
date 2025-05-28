53252e72108b03f044165c0c8eaf65a409c53753
TuProfePersonal <martin.revello@gmail.com>
2025-05-28 09:24:57 -0300

    🎸 Ajustes Cuanticos

--- run_kali_rocker.py
+++ run_kali_rocker.py
@@ -4,7 +4,7 @@ qemu-system-x86_64 \
   -m 4096 \
   -smp 4 \
   -enable-kvm \
-  -drive file=~/KVMs/Disks/kali_rocker_cuantico.qcow2,format=qcow2 \
+  -drive file=~/KVMs/Disks/kali-rocker.qcow2,format=qcow2 \
   -vga virtio \
   -display default,show-cursor=on \
   -net nic -net user \
